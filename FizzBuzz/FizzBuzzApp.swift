a9077db54c5ed0c4c0c35fe01527cf50704a1bb0
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-11-28 22:58:52 +0900

    Initial Commit

---
+++ FizzBuzz/FizzBuzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  FizzBuzzApp.swift
+//  FizzBuzz
+//
+//  Created by joe on 11/28/24.
+//
+
+import SwiftUI
+
+@main
+struct FizzBuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
