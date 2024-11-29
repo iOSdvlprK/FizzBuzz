cdef6a56452d365bceb103023390f27f327aebf1
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-11-29 23:48:38 +0900

    the about view and background image view

---
+++ FizzBuzz/View/Main Views/AboutView.swift
@@ -0,0 +1,31 @@
+//
+//  AboutView.swift
+//  FizzBuzz
+//
+//  Created by joe on 11/29/24.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        ZStack {
+            Color.blue.opacity(0.5).ignoresSafeArea()
+            
+            Text("""
+    Fizz buzz is a group word game for children to teach them about division. Players take turns to count incrementally, replacing any number divisible by three with the word "fizz", and any numberdivisible by five with the word "buzz".
+
+    Fizz buzz (often spelled FizzBuzz in this context) has been used as an interview screening device for computer programmers. Writing a program to output the first 100 FizzBuzz numbers is a relatively trivial problem requiring little more than a loop and conditional statements. However, its value in coding interviews is to analyze fundamental coding habits that may be indicative of overall coding ingenuity.
+""")
+            .font(.title)
+            .fontWeight(.semibold)
+            .multilineTextAlignment(.leading)
+            .minimumScaleFactor(0.1)
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    AboutView()
+}
---
+++ FizzBuzz/View/Supporting Views/BackgroundImageView.swift
@@ -0,0 +1,41 @@
+//
+//  BackgroundImageView.swift
+//  FizzBuzz
+//
+//  Created by joe on 11/29/24.
+//
+
+import SwiftUI
+
+struct BackgroundImageView: View {
+    var body: some View {
+        AsyncImage(url: URL(string: getImage()), transaction: Transaction(animation: .default)) { phase in
+            switch phase {
+            case .success(let image):
+                image
+                    .resizable()
+                    .scaledToFill()
+                    .blur(radius: 0.3)
+                    .ignoresSafeArea()
+            default:
+                Color
+                    .clear
+                    .ignoresSafeArea()
+            }
+        }
+    }
+    
+    func getImage() -> String {
+        let listOfImages = [
+            "https://code.kx.com/q/img/fizzbuzz.png",
+            "https://images.pexels.com/photos/4068379/pexels-photo-4068379.jpeg?auto=compress&cs=tinysrgb&w=1600",
+            "https://images.pexels.com/photos/6387827/pexels-photo-6387827.jpeg?auto=compress&cs=tinysrgb&w=1600"
+        ]
+        
+        return listOfImages.randomElement() ?? ""
+    }
+}
+
+#Preview {
+    BackgroundImageView()
+}
