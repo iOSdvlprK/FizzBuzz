9d49e726b0f7181bcbf82a9d4b5b04f7622d9e1c
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-11-30 23:40:09 +0900

    complete the about view

--- FizzBuzz/View/Main Views/AboutView.swift
+++ FizzBuzz/View/Main Views/AboutView.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct AboutView: View {
     var body: some View {
         ZStack {
-            Color.blue.opacity(0.5).ignoresSafeArea()
+            BackgroundImageView()
+                .opacity(0.3)
             
             Text("""
     Fizz buzz is a group word game for children to teach them about division. Players take turns to count incrementally, replacing any number divisible by three with the word "fizz", and any numberdivisible by five with the word "buzz".
@@ -21,6 +22,8 @@ struct AboutView: View {
             .fontWeight(.semibold)
             .multilineTextAlignment(.leading)
             .minimumScaleFactor(0.1)
+            .padding()
+            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.8)
             .padding()
         }
     }
--- FizzBuzz/View/Supporting Views/BackgroundImageView.swift
+++ FizzBuzz/View/Supporting Views/BackgroundImageView.swift
@@ -15,7 +15,7 @@ struct BackgroundImageView: View {
                 image
                     .resizable()
                     .scaledToFill()
-                    .blur(radius: 0.3)
+                    .blur(radius: 0.7)
                     .ignoresSafeArea()
             default:
                 Color
@@ -27,9 +27,9 @@ struct BackgroundImageView: View {
     
     func getImage() -> String {
         let listOfImages = [
-            "https://code.kx.com/q/img/fizzbuzz.png",
+//            "https://code.kx.com/q/img/fizzbuzz.png",
             "https://images.pexels.com/photos/4068379/pexels-photo-4068379.jpeg?auto=compress&cs=tinysrgb&w=1600",
-            "https://images.pexels.com/photos/6387827/pexels-photo-6387827.jpeg?auto=compress&cs=tinysrgb&w=1600"
+//            "https://images.pexels.com/photos/6387827/pexels-photo-6387827.jpeg?auto=compress&cs=tinysrgb&w=1600"
         ]
         
         return listOfImages.randomElement() ?? ""
