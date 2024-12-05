0586e31ec7946434028243e2403e5c96986993ba
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-12-05 23:05:33 +0900

    code creator view; completion of tab views

--- FizzBuzz/View/Main Views/CodeCreatorView.swift
+++ FizzBuzz/View/Main Views/CodeCreatorView.swift
@@ -6,10 +6,45 @@
 //
 
 import SwiftUI
+import UniformTypeIdentifiers
 
 struct CodeCreatorView: View {
+    @StateObject var fizzBuzzAlgViewModel = FizzBuzzAlgViewModel()
+    
     var body: some View {
-        Text("CodeCreatorView")
+        NavigationStack {
+            List(fizzBuzzAlgViewModel.fizzBuzzModels) { item in
+                NavigationLink(item.name) {
+                    ZStack {
+                        Color
+                            .gray
+                            .opacity(0.3)
+                            .ignoresSafeArea()
+                        
+                        ScrollView {
+                            VStack {
+                                Text(item.actualFizzBuzzCode)
+                                    .font(.headline)
+                                    .padding()
+                                    .minimumScaleFactor(0.1)
+                                    .contextMenu {
+                                        Button(
+                                            action: {
+                                                UIPasteboard.general.string = item.actualFizzBuzzCode
+                                            },
+                                            label: {
+                                                Text("Copy to clipboard")
+                                                Image(systemName: "doc.on.doc")
+                                            }
+                                        )
+                                    }
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Fizz Buzz")
+        }
     }
 }
 
